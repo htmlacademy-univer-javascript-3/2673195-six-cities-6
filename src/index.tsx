74fed8618213dbc461be12dcf6b63f8863848fed
vladr <vladreva57@gmail.com>
2025-09-25 03:17:21 +0500

    refactor: moved Settings enum to separate file

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App.tsx';
-
-const Settings = {
-  CityCardsAmount: 5,
-} as const;
+import {Settings} from './settings.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ src/settings.tsx
@@ -0,0 +1,3 @@
+export const Settings = {
+  CityCardsAmount: 5,
+} as const;
