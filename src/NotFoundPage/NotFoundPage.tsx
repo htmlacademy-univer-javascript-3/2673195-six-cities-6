21968aadb9f1dd83bb419e8dddb32f7b42da475b
vladr <vladreva57@gmail.com>
2025-10-02 14:57:12 +0500

    feat: add link to main page from NotFoundPage

--- src/NotFoundPage/NotFoundPage.tsx
+++ src/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,16 @@
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../const.ts';
+
 export function NotFoundPage(){
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       <h1>404</h1>
       <p>Извините, странице по такому адресу не найдена</p>
-      <p>Пожалуйста проверьте адрес, или перейдите на главную страницу</p>
+      <p>
+        <Link to={AppRoute.Main} style={{textDecoration: 'underline', color: 'blue', fontStyle: 'italic'}}>
+          Вернутся на главную страницу
+        </Link>
+      </p>
     </div>
   );
 }
