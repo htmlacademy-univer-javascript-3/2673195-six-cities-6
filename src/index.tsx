4790788d627882bcbae79abba347ef9915f177f7
Владислав Рева <Vladislav.Reva@urfu.me>
2025-10-16 01:22:02 +0500

    refactor: moved reviews to reviews.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App.tsx';
-import {offers, reviews} from './mocks/offers.ts';
+import {offers} from './mocks/offers.ts';
 import {users} from './mocks/users.ts';
+import {reviews} from './mocks/reviews.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -1,4 +1,4 @@
-import {OfferDTO, OfferReview} from '../types/offerDTO.ts';
+import {OfferDTO} from '../types/offerDTO.ts';
 
 export const offers: OfferDTO[] = [
   {
@@ -65,12 +65,3 @@ export const offers: OfferDTO[] = [
   },
 ];
 
-export const reviews: OfferReview[] = [
-  {
-    id: 0,
-    stars: 4,
-    text: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.\n',
-    authorId: 1,
-    date: new Date(2019, 4, 1),
-  }
-];
