2c54fc970fdf2967aea650f97d4df4328b7bd6e6
Владислав Рева <Vladislav.Reva@urfu.me>
2025-12-03 20:34:02 +0500

    feat: add createSelector for optimization in offers and favourites selectors

--- src/store/slices/favourites/favouritesSelectors.ts
+++ src/store/slices/favourites/favouritesSelectors.ts
@@ -1,12 +1,17 @@
 import {State} from '../../../types/state.ts';
 import {NameSpace} from '../../../const.ts';
 import {OffersList} from '../../../types/responses/offers/offersList.ts';
+import {createSelector} from '@reduxjs/toolkit';
+import {getCityName} from '../city/citySelectors.ts';
 
-export const getFavouritesInCity = (state: Pick<State, NameSpace.Favourites | NameSpace.City>): OffersList => {
-  const favourites = state[NameSpace.Favourites].favourites;
-  const city = state[NameSpace.City].cityName;
-  return favourites.filter((x) => x.city.name === city);
-};
+const getFavourites = (state: Pick<State, NameSpace.Favourites>): OffersList =>
+  state[NameSpace.Favourites].favourites;
+
+export const getFavouritesInCity = createSelector(
+  [getFavourites, getCityName],
+  (favourites, cityName) =>
+    favourites.filter((x) => x.city.name === cityName)
+);
 
 export const getFavouritesLoadingStatus = (state: Pick<State, NameSpace.Favourites>):
    boolean => state[NameSpace.Favourites].isFavouritesDataLoading;
--- src/store/slices/user/userSelectors.ts
+++ src/store/slices/user/userSelectors.ts
@@ -3,5 +3,6 @@ import {AuthorizationStatus, NameSpace} from '../../../const.ts';
 
 export const getAuthorizationStatus = (state: Pick<State, NameSpace.User>):
   AuthorizationStatus => state[NameSpace.User].authorizationStatus;
+
 export const getAuthCheckedStatus = (state: Pick<State, NameSpace.User>):
   boolean => state[NameSpace.User].authorizationStatus !== AuthorizationStatus.Unknown;
